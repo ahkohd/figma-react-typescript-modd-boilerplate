1624ca8dfe8fe895463f60a982ec9697a7d483d8
Victor Aremu <victor.olorunbunmi@gmail.com>
2020-11-28 21:01:20 +0100

    👌 IMPROVE: import

--- src/ui/ui.tsx
+++ src/ui/ui.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from '@assets/svgs/logo.svg';
 import './ui.scss';
 import useStore from '@store/store';
